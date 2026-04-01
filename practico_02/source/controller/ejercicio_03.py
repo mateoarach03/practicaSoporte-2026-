b24557de77468f6264a0a3c39294bfa9b7dee374
IgnacioAmelioO <nameliortiz@gmail.com>
2026-04-01 17:34:57 -0300

    Resolucion practico 2

--- practico_02/source/controller/ejercicio_03.py
+++ practico_02/source/controller/ejercicio_03.py
@@ -12,6 +12,12 @@ $PATH$/practico_02> python -m source.controller.ejercicio_03
 """
 
 # Completar
+import main as main
+import source.util as util
+import source.controller.controller as controller
+import config.test_config as test_config
+import source.data.database as database
+import config.ci_cd.deploy_travis as deploy_travis
 
 # NO MODIFICAR - INICIO
 assert main.name == "main"
